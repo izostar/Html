8c2df1cea9de27a6c08fb741a25d377e0a1ad139
izostar <izostar@gmail.com>
2020-12-16 20:04:11 +0100

    Add files via upload

---
+++ Glavni.java
@@ -0,0 +1,11 @@
+
+public class Glavni {
+
+public static void main(String[] args) {
+    System.out.println("Dela ...");
+
+    for (String str: args) {
+        System.out.println(str);
+    }
+}
+}
